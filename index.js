884ba444215ee86bfcb65877de5cbfb215298758
Michael Perrotte <michael.perrotte@resolver.com>
2018-08-07 00:09:17 -0400

    WIP: added index file

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const pino = require('express-pino-logger');
+
+const logger = require('pino')({
+  level: process.env.LOG_LEVEL || 'info',
+});
+
+const app = express();
+
+app.use(pino);
+
+const { APP_PORT, } = process.env;
+
+if (!APP_PORT) {
+  logger.error('missing.environment:app_port');
+}
+
+app.listen(APP_PORT, () => {
+  logger.info(`Stargate Listening on port: ${APP_PORT}`);
+});
